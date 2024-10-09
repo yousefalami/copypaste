2d7c15d9144dc636e543ba84f2491b818d9cee13
Yousef Alami <114333627+yousefalami@users.noreply.github.com>
2024-10-09 12:22:48 +0330

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,7 +1,6 @@
-// Import node-fetch
-const fetch = require('node-fetch');
+// Use import instead of require
+import fetch from 'node-fetch';
 
-// Your URL
 const url = 'https://api.adsgram.ai/event?type=reward&trackingtypeid=14&record=IiRmNzEyMTYxMi1iY2Q4LTQ4NTEtYTE0ZC1iMGZjMjRlNDg5ZDEqBDgzMjkyBTEwNTQ1OgU4NzcxOUDLlY-4BkoJOTQ0NDE4MjMyUgQzODUyWhpodHRwczovL2JvdC50b25jaXJjbGUub3JnL2IBMmoDd2VicgJlbooBBDMyMTKSAQQ3MjY0mgEMNDM4NTkuNTAwMDAwqgEIMC40NTc3MDGyAQEyugEMNTQuMTcwLjY5LjI5wgECaWU';
 
 // Function to make money
